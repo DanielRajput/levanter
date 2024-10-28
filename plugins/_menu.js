215e9c98d819f13b561a14ffcc844be98528f4b3
KING B2K ✓ <138299021+DanielRajput@users.noreply.github.com>
2024-10-28 13:04:23 +0530

    Update _menu.js

--- plugins/_menu.js
+++ plugins/_menu.js
@@ -21,7 +21,7 @@ bot.addCommand(
     })
     const [date, time] = getDate()
     let CMD_HELP = `╭────────────────╮
-						ʟᴇᴠᴀɴᴛᴇʀ
+						KING B2K BOT ✓
 ╰────────────────╯
 
 ╭────────────────
@@ -90,7 +90,7 @@ bot.addCommand(
       }
     })
     const [date, time] = getDate()
-    let msg = `\`\`\`╭═══ LEVANTER ═══⊷
+    let msg = `\`\`\`╭═══ KING B2K ✓ ═══⊷
 ┃❃╭──────────────
 ┃❃│ Prefix : ${ctx.PREFIX}
 ┃❃│ User : ${message.pushName}
